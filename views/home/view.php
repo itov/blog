ed72b345bfd5b721aea5b1cce27aed1fce38b323
itov <borisitov@gmail.com>
2016-08-29 16:57:36 +0300

    base

---
+++ views/home/view.php
@@ -0,0 +1,11 @@
+<?php $this->title = $this->post['title']; ?>
+<main>
+<h1><?=htmlspecialchars($this->title)?></h1>
+<p>
+    <i>Posted on</i>
+    <?=htmlentities($this->post['date'])?>
+    <i>by</i>
+    <?=htmlentities($this->post['full_name'])?>
+</p>
+<p><?=$this->post['content']?></p>
+</main>
---
+++ views/users/register.php
@@ -0,0 +1,15 @@
+<?php $this->title = 'Register New User'; ?>
+
+<h1><?= htmlspecialchars($this->title) ?></h1>
+
+<form method="post">
+    <div>Username: 
+        <input type="text" name="username" required /></div>
+    <div>Password: 
+        <input type="password" name="password" required /></div>
+    <div>Password Confirm: 
+        <input type="password" name="password_confirm" required /></div>
+    <div>Full name: 
+        <input type="text" name="full_name" /></div>
+    <div><input type="submit" value="Login"></div>
+</form>
