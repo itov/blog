7a4ac9807ebe9539ba23b319566b9ac8293e1140
itov <borisitov@gmail.com>
2016-08-31 17:40:53 +0300

    databasess

---
+++ users.sql
@@ -0,0 +1,6 @@
+INSERT INTO blog.users (username, password_hash, full_name) VALUES ('bobi', '$2y$10$ozZV9sB48bU38CvLhAAgQufkgLfPQl1iije8D51ATPocaUJw6o73C', 'Boris Itov');
+INSERT INTO blog.users (username, password_hash, full_name) VALUES ('vesi', '$2y$10$ozZV9sB48bU38CvLhAAgQufkgLfPQl1iije8D51ATPocaUJw6o73C', 'Vesi Itova');
+INSERT INTO blog.users (username, password_hash, full_name) VALUES ('admin', '$2y$10$ozZV9sB48bU38CvLhAAgQufkgLfPQl1iije8D51ATPocaUJw6o73C', null);
+INSERT INTO blog.users (username, password_hash, full_name) VALUES ('guest', '$2y$10$ozZV9sB48bU38CvLhAAgQufkgLfPQl1iije8D51ATPocaUJw6o73C', null);
+INSERT INTO blog.users (username, password_hash, full_name) VALUES ('ani', '$2y$10$ozZV9sB48bU38CvLhAAgQufkgLfPQl1iije8D51ATPocaUJw6o73C', 'Ani');
+INSERT INTO blog.users (username, password_hash, full_name) VALUES ('qweqwe', '$2y$10$ozZV9sB48bU38CvLhAAgQufkgLfPQl1iije8D51ATPocaUJw6o73C', 'qweqwe qweqwe');
